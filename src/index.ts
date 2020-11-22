88b698591ec436343c01689ba22834bc68da7cb2
Zachary Ernst <zernst3@live.com>
2020-11-21 21:19:39 -0500

    Trying these cors settings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { PORT } from "./config/constants";
-// const cors = require("cors");
+const cors = require("cors");
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -9,16 +9,20 @@ const socketio = require("socket.io");
 const router = require("./routes/router");
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    allRoutes: true,
-    origin: "https://zernst-typescript-chat-app-s.herokuapp.com/",
-    methods: ["GET", "POST"],
+    // allRoutes: true,
+    // origin: "*",
+    // methods: ["GET", "POST"],
     // allowedHeaders: ["my-custom-header"],
     // credentials: true,
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   },
 });
 
